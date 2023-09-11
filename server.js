247148dc354758b1760893129032452506167ba9
Sourav Kumar Gupta <89971045+SouravKrGupta@users.noreply.github.com>
2023-09-11 17:16:47 +0530

    update

--- server.js
+++ server.js
@@ -21,17 +21,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
-
+app.use(express.static(path.join(__dirname, "./client/build")));
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-//rest api
-app.get("/", (req, res) => {
-  res.send("<h1>Welcome to ecommerce app</h1>");
-});
-app.use(express.static(path.join(__dirname, "./client/build")));
+
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/bulid/index.html"));
 });
