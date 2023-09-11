ece71badaa6c0eb546ccff93ebb3816dd681fac1
Sourav Kumar Gupta <89971045+SouravKrGupta@users.noreply.github.com>
2023-09-11 17:20:52 +0530

    update

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use("/api/v1/product", productRoutes);
 
 
 app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./client/bulid/index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 //PORT
 const PORT = process.env.PORT || 8080;
